038545ea4844998fcddf19c3acd8399d78d9c929
nicole <nzhou30@stuy.edu>
2022-12-08 13:20:27 -0500

    tabs

---
+++ app/js/home.js
@@ -0,0 +1,20 @@
+function openTab(evt, tabAction) {
+    // Declare variables
+    var i, tabcontent, tablinks;
+
+    // Hide all elements with class="tabcontent"
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove class "active" from all elements with class="tablinks"
+    tablinks = document/getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show current tab and add an "active" class to the button that opened the tab
+    document.getElementById(tabAction).style.display = "block";
+    evt.currentTarget.className += " active";
+}
