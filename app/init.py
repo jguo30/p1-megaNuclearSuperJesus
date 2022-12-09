fe1dea0c8df843ebbc9ffecb436cabb95b7dc7eb
samc5 <scowan30@stuy.edu>
2022-12-08 19:06:05 -0500

    ttt

--- app/init.py
+++ app/init.py
@@ -4,10 +4,13 @@ code = 7997
 app = app = Flask(__name__)      
 @app.route("/", methods = ["POST", "GET"])                   
 def display():
+    return render_template("main.html")
+@app.route("/results", methods = ["POST", "GET"])
+def results():
     lat = 40
     lon = -74
     dist = 50
-    #code = request.form["Code"]
+    code = int(request.form["Code"])
     num = 5
 
     url = f'http://spatial.virtualearth.net/REST/v1/data/Microsoft/PointsOfInterest?spatialFilter=nearby({lat},{lon},{dist})&$filter=EntityTypeID%20eq%20%27{code}%27&$select=EntityID,DisplayName,Latitude,Longitude,__Distance&$top={num}&$format=json&key=Aq5RfNwj-YFePBBwOI4Dz18rk5AcP_hJ9BcR8g91kQUZNzWY_eNYJT3f79zkfHU0'
@@ -22,12 +25,7 @@ def display():
         #print(i)
         results.append(i["DisplayName"])
         #print(i["DisplayName"])
-    
-
-
-
-
-    return render_template("main.html", poi = results)
+    return render_template("results.html", poi = results)
 @app.route("/code", methods = ["POST", "GET"])                   
 def code():
     code = int(request.form["Code"])
