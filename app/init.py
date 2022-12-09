01504b085937b7da574776a2464869de585cdc83
samc5 <scowan30@stuy.edu>
2022-12-08 20:04:49 -0500

    input of latitude, longitude, POI code, returns a page with up to five closest matches within 50 miles of the location

--- app/init.py
+++ app/init.py
@@ -7,8 +7,8 @@ def display():
     return render_template("main.html")
 @app.route("/results", methods = ["POST", "GET"])
 def results():
-    lat = 40
-    lon = -74
+    lat = int(request.form["Lat"])
+    lon = int(request.form["Long"])
     dist = 50
     code = int(request.form["Code"])
     num = 5
